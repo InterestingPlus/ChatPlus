e119a2bc10bbd1c43f1908619f95b5494011cb7e
Jatin Poriya <111553605+InterestingPlus@users.noreply.github.com>
2025-05-04 14:13:19 +0530

    Delete routes directory

--- routes/messagesRoute.js
+++
@@ -1,8 +0,0 @@
-const { getMessage, addMessage } = require("../controllers/messagesController");
-
-const router = require("express").Router();
-
-router.post("/getmsg", getMessage);
-router.post("/addmsg", addMessage);
-
-module.exports = router;
